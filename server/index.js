8ee797ab78ed23cbe1999d5a22fb8ed2de1c44b0
Sumit <sumit@logiq.ai>
2021-10-16 16:58:22 +0530

    adding changes in server code

--- server/index.js
+++ server/index.js
@@ -1,33 +1,22 @@
 const logger = require('./logger').logger
 const  {Server} = require('socket.io')
-const crypto = require('crypto')
+
 
 const io = new Server()
 
-const socketMap = {}
-
-
-const addToMap = (socket) => {
-    const {id} = socket;
-    const hash = crypto.createHash('md5').update(id).digest("hex");
-    socket.join(hash)
-    socketMap[hash] = id;
-    io.to(hash).emit(`connected to rooom ${hash}`)
-    console.log({socketMap})
-}
 
 
 io.on('connection', (socket) =>{
     socket.on("join", (id)=>{
-        console.log(id)
+        logger.info(`Joining room ${id}`)
         socket.join(id);
         io.sockets.in(id).emit("response", `this is for id: ${id}`)
     })
 
 
-    socket.on("data", id=>{
-        console.log("from id ", id)
-        io.sockets.in(id).emit("response ", `data recv from ${id}`)
+    socket.on("data", data=>{
+        console.log(data)
+        
     })
 })
 
